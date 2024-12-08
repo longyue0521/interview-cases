33c48fea35d3f2e78891fbd217cea1b744615fb5
zwl <1633720889@qq.com>
2024-12-08 20:08:29 +0800

    修改case30

--- case21_30/case30/case30_test.go
+++ case21_30/case30/case30_test.go
@@ -13,10 +13,13 @@ import (
 	"time"
 )
 
-type Case30 struct {
-	ID     int64 `gorm:"primaryKey;autoIncrement"` // 设置为主键
-	Name   string
-	Status int32
+type User struct {
+	ID   int64 `gorm:"primaryKey;autoIncrement"` // 设置为主键
+	Name string
+	// 积分
+	Credit float64
+	// 版本
+	Version int32
 }
 
 type TestSuite struct {
@@ -27,20 +30,22 @@ type TestSuite struct {
 func (t *TestSuite) SetupSuite() {
 	t.db = test.InitDB()
 	// 借助 GORM 来初始化表结构
-	err := t.db.AutoMigrate(&Case30{})
+	err := t.db.AutoMigrate(&User{})
 	require.NoError(t.T(), err)
 	// 初始化一些数据
 	t.prepareData()
 }
 
 func (t *TestSuite) TestTableLock() {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
 	t.db.Transaction(func(tx *gorm.DB) error {
 		// select for update 查找一个不存在列会导致锁表
-		var ca Case30
-		err := tx.WithContext(context.Background()).
+		var us User
+		err := tx.WithContext(ctx).
 			Clauses(clause.Locking{Strength: "UPDATE"}).
-			Model(&Case30{}).
-			Where("id = ?", 20000).First(&ca).Error
+			Model(&User{}).
+			Where("id = ?", 20000).First(&us).Error
 		if err != nil {
 			if errors.Is(err, gorm.ErrRecordNotFound) {
 				// 稍微把时间调长一点方便观察表锁现象
@@ -50,37 +55,47 @@ func (t *TestSuite) TestTableLock() {
 				return err
 			}
 		}
-		// 如果存在，就更新
-		tx.WithContext(context.Background()).Where("id = ?", 20000).
-			Update("status", 1)
-		return nil
+		if us.Credit > 100 {
+			// 如果积分大于100,就扣100分
+			tx.WithContext(ctx).Where("id = ?", 20000).
+				Model(&User{}).
+				Update("credit", gorm.Expr("credit - ?", 100))
+			return nil
+		} else {
+			return errors.New("积分不足")
+		}
 	})
 }
 
 func (t *TestSuite) TestCas() {
-	var ca Case30
-	err := t.db.WithContext(context.Background()).Create(&Case30{
-		ID:     20000,
-		Status: 0,
-	}).Error
-	require.NoError(t.T(), err)
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
 	for {
-		err := t.db.WithContext(context.Background()).
-			Model(&Case30{}).
-			Where("id = ? ", 20000).First(&ca).Error
+		var us User
+		err := t.db.WithContext(ctx).
+			Model(&User{}).
+			Where("id = ? ", 1000).First(&us).Error
 		if err != nil {
 			return
 		}
 		// 找到就更新
-		res := t.db.WithContext(context.Background()).
-			Model(&Case30{}).
-			Where("id = ? and status = ?", 20000, ca.Status).
-			Update("status", 1)
-		if res.Error != nil {
+		if us.Credit > 100 {
+			res := t.db.WithContext(ctx).
+				Model(&User{}).
+				Where("id = ? and version = ? ", 1000, us.Version).
+				Updates(map[string]any{
+					"credit":  gorm.Expr("credit - ?", 100),
+					"version": us.Version + 1,
+				})
+			if res.RowsAffected == 0 {
+				// 更新失败，说明可能有并发操作，比如说用户开了好几个页面一起兑换礼物，想要找你的漏洞
+				continue
+			}
+			// 更新成功
 			return
-		}
-		if res.RowsAffected > 0 {
-			// 说明修改成功
+
+		} else {
+			// 不足100就返回
 			return
 		}
 	}
@@ -91,11 +106,13 @@ func (t *TestSuite) prepareData() {
 	// 先清空数据
 	err := t.db.WithContext(context.Background()).Exec("DELETE FROM users;").Error
 	require.NoError(t.T(), err)
-	users := make([]*Case30, 0, 32)
+	users := make([]*User, 0, 32)
 	// 插入1000条数据
 	for i := 1; i <= 1000; i++ {
-		u := &Case30{
-			Name: fmt.Sprintf("user_%d", i),
+		u := &User{
+			ID:     int64(i),
+			Name:   fmt.Sprintf("user_%d", i),
+			Credit: 1000,
 		}
 		users = append(users, u)
 	}
